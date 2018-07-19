c95748ed1035e086c23fb647e767fffd529c8d3f
Quan <quank54utc@gmail.com>
2018-07-19 14:12:32 +0700

    escape excerpt more

--- inc/extras.php
+++ inc/extras.php
@@ -39,7 +39,7 @@ function autodealer_excerpt_more_link( $link ) {
 		'<p class="link-more"><a href="%1$s" class="more-link">%2$s</a></p>',
 		esc_url( get_permalink( get_the_ID() ) ),
 		/* translators: %s: Name of current post */
-		sprintf( __( 'Read More<span class="screen-reader-text"> "%s"</span>', 'autodealer' ), get_the_title( get_the_ID() ) )
+		wp_kses_post( sprintf( __( 'Read More<span class="screen-reader-text"> "%s"</span>', 'autodealer' ), get_the_title( get_the_ID() ) ) )
 	);
 	return ' [&hellip;] ' . $link;
 }
