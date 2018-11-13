db4bd84b5bd6799cc25e4148e796acd1616962bf
viet nguyen <viet34tqc@gmail.com>
2018-11-13 09:50:08 +0700

    Style lại at a glance trang single listing

--- listings/single-listing/at-a-glance.php
+++ listings/single-listing/at-a-glance.php
@@ -19,19 +19,32 @@ if ( ! carlistings_is_plugin_active() ) {
 
 <div class="at-a-glance">
 	<h3><?php esc_html_e( 'Features Highlight', 'carlistings' ); ?></h3>
-	<ul>
-	<?php if ( auto_listings_odometer() ) { ?>
-		<li class="odomoter"><i class="icofont icofont-speed-meter"></i> <?php echo esc_html( auto_listings_odometer() ); ?></li>
-	<?php } ?>
-	<?php if ( auto_listings_transmission() ) { ?>
-		<li class="transmission"><i class="icofont icofont-ui-settings"></i> <?php echo esc_html( auto_listings_transmission() ); ?></li>
-	<?php } ?>
-	<?php if ( auto_listings_body_type() ) { ?>
-		<li class="body"><i class="icofont icofont-car-alt-4"></i> <?php echo auto_listings_body_type(); // WPCS: XSS OK. ?></li>
-	<?php } ?>
-	<?php if ( auto_listings_engine() ) { ?>
-		<li class="vehicle"><?php echo esc_html( auto_listings_engine() ); ?></li>
-	<?php } ?>
-	</ul>
-
+	<table>
+		<tbody>
+			<tr>
+				<td>
+					<?php if ( auto_listings_odometer() ) { ?>
+						<span class="odomoter"><i class="icofont icofont-speed-meter"></i> <?php echo esc_html( auto_listings_odometer() ); ?></span>
+					<?php } ?>
+				</td>
+				<td>
+					<?php if ( auto_listings_transmission() ) { ?>
+						<span class="transmission"><i class="icofont icofont-ui-settings"></i> <?php echo esc_html( auto_listings_transmission() ); ?></span>
+					<?php } ?>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<?php if ( auto_listings_body_type() ) { ?>
+						<span class="body"><i class="icofont icofont-car-alt-4"></i> <?php echo auto_listings_body_type(); // WPCS: XSS OK. ?></span>
+					<?php } ?>
+				</td>
+				<td>
+					<?php if ( auto_listings_engine() ) { ?>
+						<span class="vehicle"><?php echo esc_html( auto_listings_engine() ); ?></span>
+					<?php } ?>
+				</td>
+			</tr>
+		</tbody>
+	</table>
 </div>
