243919d3f570e292df7d97318eb9b51b522ad3d4
ToanNguyen94 <nguyenthetoan.ntt@gmail.com>
2023-01-30 09:02:43 +0700

    Thêm code hiển thị Header-Image

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -21,7 +21,7 @@ function carlistings_custom_header_setup() {
 		apply_filters(
 			'carlistings_custom_header_args',
 			array(
-				'default-image'      => '',
+				'default-image'      => get_template_directory_uri() . '/images/page-header.png',
 				'default-text-color' => '000000',
 				'width'              => 1000,
 				'height'             => 250,
@@ -30,6 +30,13 @@ function carlistings_custom_header_setup() {
 			)
 		)
 	);
+	register_default_headers( array(
+		'linux-server' => array(
+			'url'           => '%s/images/page-header.png',
+			'thumbnail_url' => '%s/images/page-header.png',
+			'description'   => esc_html__( 'Linux Server', 'carlistings' ),
+		),
+	) );
 }
 add_action( 'after_setup_theme', 'carlistings_custom_header_setup' );
 
@@ -41,19 +48,19 @@ if ( ! function_exists( 'carlistings_header_style' ) ) :
 	 */
 	function carlistings_header_style() {
 		$header_text_color = get_header_textcolor();
-
-		/*
-		 * If no custom options for text are set, let's bail.
-		 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
-		 */
-		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
-			return;
-		}
+		$header_image      = get_header_image();
 
 		// If we get this far, we have custom styles. Let's do this.
 		?>
 		<style type="text/css">
 		<?php
+		if ( has_header_image() ) :
+			?>
+			.page-header {
+				background-image: url( <?= esc_url( $header_image ); ?> );
+			}
+			<?php
+		endif;
 		// Has the text been hidden?
 		if ( ! display_header_text() ) :
 			?>
